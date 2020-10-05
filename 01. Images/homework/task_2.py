48717f565148439b3ac4d773158e4bb602f902d4
smileamaxdev <novoseltsev.as@phystech.edu>
2020-10-06 00:28:05 +0300

    task2 with roads and cars done

--- 01. Images/homework/task_2.py
+++ 01. Images/homework/task_2.py
@@ -1,6 +1,31 @@
 import cv2
 import numpy as np
 
+def find_roads(image: np.ndarray) -> tuple:
+    
+    """
+    Функция определяющая количество и диапозон дорожек по характерному цвету разделителей
+
+    :param image: исходное изображение
+    :return: координаты старта и конца дорожек
+    """
+    
+    coords = np.where(image[0] == [255, 252, 121])[0]    # Найдем все пиксели в строке с характерным желтым цветом
+    value  = coords[0]
+    start  = []
+    end    = []
+    
+    """
+    Будем искать в полученных координатах характерные изменения в значениях для сепарации дорожек
+    """
+    for i in range(0, coords.shape[0] - 1):
+        if coords[i] - value > 5:
+            start.append(coords[i - 1])
+            end.append(coords[i])
+        value = coords[i]
+        
+    return (start, end)
+
 
 def find_road_number(image: np.ndarray) -> int:
     """
@@ -9,9 +34,18 @@ def find_road_number(image: np.ndarray) -> int:
     :param image: исходное изображение
     :return: номер дороги, на котором нет препятсвия на дороге
     """
+    start, end = find_roads(image)   # Найдем количество и диапозон дорожек
+    
     road_number = None
-    # Ваш код тут
-    pass
-    # Ваш код тут
+    
+    """
+    В полученных диапозонах будем искать количество характерных красных объектов.
+    Для устранения шумовых значений от границ дорожек будем рассматривать немного смещенную область
+    """
+    
+    for x, y, i in zip(start, end, range(0, len(start))):
+        
+        if len(np.where( image[:, x + 20 : y - 20] == [255, 0, 0] )[2]) == 0:
+            road_number = i
 
     return road_number
